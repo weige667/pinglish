7409b8da919bb35464e782625b8cf379e901b500
weige667 <17346715@qq.com>
2026-08-09 03:07:50 +0000

    Add src/views/HomeView.vue

---
+++ src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="view-home">
+    <div class="card hero-card">
+      <h1>Pinglish</h1>
+      <p class="subtitle">乒乓球主题英语训练</p>
+    </div>
+    <div class="card">
+      <p>模块开发中，敬请期待。</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+</script>
