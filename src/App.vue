48b6046c23e3c9ebe48e1b7ccaec1f967b918b54
weige667 <17346715@qq.com>
2026-08-09 03:07:42 +0000

    Add src/App.vue

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app-container">
+    <NavBar />
+    <main class="main-content">
+      <router-view v-slot="{ Component }">
+        <transition name="fade" mode="out-in">
+          <component :is="Component" />
+        </transition>
+      </router-view>
+    </main>
+  </div>
+</template>
+
+<script setup>
+import NavBar from './components/NavBar.vue'
+</script>
